a85d35f51dad10f3f8a5c8c93bc45948b430159a
Nathan Morrow <nathanmorrow123@gmail.com>
2023-12-04 13:43:01 -0500

    Update GPS_SIM.py

--- GPS_SIM.py
+++ GPS_SIM.py
@@ -9,8 +9,8 @@ ROTATION_EARTH=7.2921150e-5 #rad/s
 GRAV_CONSTANT=6.67430e-11 #N*m^2/kg^2
 EARTH_MASS=5.97219e24 #kg
 ANTENNA_LOCATION = [39.765659548785884, -84.19151722210734] #Latitude/Longitude in degrees
-
-def run3dTrackingSim(times,satPath1,tof,mps):
+#Assuming antenna is normal to earth surface
+def run3dTrackingSim(times,orbit_Planes,tof,mps):
 	#Create Antenna (radius,theta,phi) Starting Antenna Position
 	ant = [RADIUS_EARTH,np.radians(ANTENNA_LOCATION[0]),np.radians(ANTENNA_LOCATION[1])]
 	fig = plt.figure()
@@ -19,12 +19,6 @@ def run3dTrackingSim(times,satPath1,tof,mps):
 	plt.show()
 	for t in times:
 		start = time.time()
-		satX = satPath1[t,0]
-		satY = satPath1[t,1]
-		satZ = satPath1[t,2]
-		#SignalStrength array (xSignal,ySignal,zSignal)
-		sigStr = -(100)/updateAntennaStrength(ant,satX,satY,satZ)
-		ant = updateAntennaPosition(ant,t,mps)
 		ax.clear()
 		ax.set_xlabel("X axis")
 		ax.set_ylabel("Y Axis")
@@ -32,10 +26,16 @@ def run3dTrackingSim(times,satPath1,tof,mps):
 		ax.set_xlim([-3*RADIUS_EARTH,3*RADIUS_EARTH])
 		ax.set_ylim([-3*RADIUS_EARTH,3*RADIUS_EARTH])
 		ax.set_zlim([-3*RADIUS_EARTH,3*RADIUS_EARTH])
-		plotSatellite(ax,satX,satY,satZ,satPath1)
-		plotAntennaToSat(ax,ant,satX,satY,satZ)
-		plotAntenna(ax,ant,sigStr)
+		ant = updateAntennaPosition(ant,t,mps)
 		plotEarth(ax,t,mps)
+		for orbit_plane in orbit_Planes:
+			satX = orbit_plane[t,0]
+			satY = orbit_plane[t,1]
+			satZ = orbit_plane[t,2]
+			sigStr = -(100)/updateAntennaStrength(ant,satX,satY,satZ)#SignalStrength array (xSignal,ySignal,zSignal)
+			plotAntenna(ax,ant,sigStr)
+			plotSatellite(ax,satX,satY,satZ,orbit_plane)
+			plotAntennaToSat(ax,ant,satX,satY,satZ)
 		plt.pause(1e-3)
 		end = time.time()
 		while (end-start<1e-2):
@@ -55,17 +55,19 @@ def updateAntennaStrength(ant,satX,satY,satZ):
 	return antStr
 
 #Set orbit of L1 GPS satillite using ECEI frame 
-def setSatellitePath(tof,mps,alt,rot):
+def setSatellitePath(tof,mps,alt,theta,phi):
 	times = range(int(tof*mps))#number of points to measure in a 10 second timespan
 	times = np.divide(times,mps)
 	r = alt+RADIUS_EARTH
 	orbPeriod = 2*math.pi*math.sqrt(r**3/(GRAV_CONSTANT*EARTH_MASS))#Keeplers Equation
 	satPosMat = []
-	c, s = np.cos(rot), np.sin(rot)
-	R = np.matrix([[c, 0, s], [0, 1, 0], [-s, 0, c]])
+	c1, s1 = np.cos(theta), np.sin(theta)
+	c2, s2 = np.cos(phi), np.sin(phi)
+	R1 = np.matrix([[1, 0, 0], [0, c2, -s2], [0, s2, c2]])
+	R2 = np.matrix([[c1, 0, s1], [0, 1, 0], [-s1, 0, c1]])
 	for t in times:
 		v= np.matrix([[r * math.cos(-t/orbPeriod)],[r * math.sin(-t/orbPeriod)],[0]])
-		satPosMat.append((R*v).T)
+		satPosMat.append((R2*(R1*v)).T)
 	satPath = np.stack(satPosMat)
 	print(satPath)
 	return satPath
@@ -81,27 +83,34 @@ def plotEarth(ax,t,mps):
 	ax.plot_wireframe(postImage[0], postImage[1], postImage[2], color="b")
 def plotAntenna(ax,ant,sigStr):
 	antCart = sphericalTocartesian(ant)
-	ax.quiver(antCart[0],antCart[1],antCart[2],sigStr*antCart[0],sigStr*antCart[1],sigStr*antCart[2],color = 'purple')#xAxis Rx Antenna 
+	ax.quiver(antCart[0],antCart[1],antCart[2],sigStr*antCart[0],sigStr*antCart[1],sigStr*antCart[2],color = 'green')#normal Axis Rx Antenna 
 def plotAntennaToSat(ax,ant,satX,satY,satZ):
 	antCart = sphericalTocartesian(ant)
 	ax.quiver(antCart[0],antCart[1],antCart[2],satX, satY, satZ, color = 'black', length = 0.5)#Tx Antenna 
 def plotSatellite(ax,satX,satY,satZ,satPath1):
-	ax.scatter(satX, satY, satZ, c='red', marker='.', s=20)
-	ax.scatter(satPath1[:,0],satPath1[:,1],satPath1[:,2], c='red', marker='.', s=10)
+	ax.scatter(satX, satY, satZ, c='black', marker='.', s=20)
+	ax.scatter(satPath1[:,0],satPath1[:,1],satPath1[:,2], c='red', marker='.', s=1)
 def sphericalTocartesian(sCoords): #sCoords (rad,theta,phi)
 	cartX=sCoords[0]*math.sin(sCoords[1])*math.cos(sCoords[2])
 	cartY=sCoords[0]*math.sin(sCoords[1])*math.sin(sCoords[2])
 	cartZ=sCoords[0]*math.cos(sCoords[1])
 	return [cartX,cartY,cartZ]
 ##Void Run Method for simulation
-tof =  10*86400 # in seconds
-mps =  1/240 # Number of Simulatred Location points per second
-satPath1 = setSatellitePath(tof,mps,2e7,np.radians(45))
-satPath2 = setSatellitePath(tof,mps,3e7,np.radians(20))	
+tof =  3*86400 # in seconds
+mps =  1/60 # Number of Simulatred Location points per second
+#Satellites paths are characterized by altitude, theta rotation, phi rotation.
+inc_ang = np.radians(55)#GNSS satellites have an inclination of 55 degrees
+satPathA = setSatellitePath(tof,mps,2e7,inc_ang,0)
+satPathB = setSatellitePath(tof,mps,2e7,inc_ang+np.radians(15),2*np.radians(15))
+satPathC = setSatellitePath(tof,mps,2e7,inc_ang+np.radians(30),2*np.radians(30))	
+satPathD = setSatellitePath(tof,mps,2e7,inc_ang+np.radians(45),2*np.radians(45))
+satPathE = setSatellitePath(tof,mps,2e7,inc_ang+np.radians(60),2*np.radians(60))	
+satPathF = setSatellitePath(tof,mps,2e7,inc_ang+np.radians(75),2*np.radians(75))
+orbit_Planes = [satPathA,satPathB,satPathC,satPathD,satPathE,satPathF]	
 while True:
 	try:
 		times = range(int(tof*mps))
-		run3dTrackingSim(times,satPath1,tof,mps)
+		run3dTrackingSim(times,orbit_Planes,tof,mps)
 	except OSError:
 		print(OSError)
 	pass
